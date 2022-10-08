8512d5e39eabe3d7c7fef09783810474614bd20d
Shubham Maurya <risjiomaujio7654@gmail.com>
2022-10-08 10:14:10 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,28 @@
+
+// =================Loader=====================
+
+onload = () => {
+    const load =document.getElementById('load')
+    setTimeout(() =>{
+        load.style.display='none'
+    },963)
+
+    
+}
+
+function loader(){
+    const load =document.getElementById('load')
+    setTimeout(() =>{
+        load.style.display='none'
+    },963)
+}
+
+
 /*==================== DARK LIGHT THEME ====================*/ 
 const themeswitchButton = document.querySelectorAll('.theme-switcher')
+const themeswitch = document.querySelector('.theme-switcher')
 
 themeswitchButton.forEach(button => {
-    console.log(button)
 
     const darkTheme = 'dark-theme'
     const iconTheme = 'bx-sun'
@@ -36,7 +56,7 @@ themeswitchButton.forEach(button => {
     })
 })
 const darkTheme = 'dark-theme'
-    const iconTheme = 'bx-sun'
+const iconTheme = 'bx-sun'
 
 // Previously selected topic (if user selected)
     const darkswitchTheme = 'dark-theme'
@@ -48,13 +68,13 @@ const darkTheme = 'dark-theme'
     
     // We obtain the current theme that the interface has by validating the dark-theme class
     const getCurrentswitchTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
-    const getCurrentswitchIcon = () => button.classList.contains(iconTheme) ?'ri-moon-line' : 'ri-sun-line'
+    const getCurrentswitchIcon = () => themeswitch.classList.contains(iconTheme) ?'ri-moon-line' : 'ri-sun-line'
     
     // We validate if the user previously chose a topic
     if (selectedswitchTheme) {
       // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
       document.body.classList[selectedswitchTheme === 'dark' ? 'add' : 'remove'](darkswitchTheme)
-      button.classList[selectedswitchIcon === 'ri-moon-line' ? 'add' : 'remove'](iconswitchTheme)
+      themeswitch.classList[selectedswitchIcon === 'ri-moon-line' ? 'add' : 'remove'](iconswitchTheme)
     }
 
 
@@ -86,12 +106,40 @@ new Sortable(finalItems, {
 
 const genrator = document.querySelector('.generate-pdf')
 genrator.addEventListener('click', () => {
+    load.style.display='block'
+    loader()
+    
     setTimeout(() => {
         document.querySelector('.creator').style.display = 'none'
         document.querySelector('.result').style.display = 'block'
-        
-    }, 639);
-    updater()
+        updater()
+    }, 720);
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 1800,
+            timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+    })
+    
+    Toast.fire({
+        icon: 'success',
+        title: 'Successfully Created '
+    })
+    document.querySelector('.swal2-popup').style.background = '#1b1a1a'
+    document.querySelector('.swal2-popup').style.color = 'white'
+    document.querySelector('.swal2-timer-progress-bar').style.background = '#bebcc5'
+    document.querySelector('.swal2-success-circular-line-left').style.background = '#1b1a1a' 
+      document.querySelector('.swal2-success-circular-line-right').style.background = '#1b1a1a' 
+      document.querySelector('.swal2-success-fix').style.background = '#1b1a1a' 
+
+     
+
+    
 })
 
 
@@ -99,14 +147,38 @@ const editor = document.querySelector('.editing-pdf')
 
 
 editor.addEventListener('click', () => {
+    load.style.display='block'
+
+    loader()
     setTimeout(() => {
         document.querySelector('.creator').style.display = 'block'
         document.querySelector('.result').style.display = 'none'
         
-    }, 639);
+        
+    }, 369);
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 1800,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+      
+      Toast.fire({
+          icon: 'info',
+          title: 'Data Retrived '
+        })
+        
+        document.querySelector('.swal2-popup').style.background = '#1b1a1a'
+        document.querySelector('.swal2-popup').style.color = 'white'
+        document.querySelector('.swal2-timer-progress-bar').style.background = '#bebcc5'
+        
+    })
     
-})
-
 function updater(){
     const listTitleData = document.getElementsByClassName('home__title')
     
@@ -115,12 +187,10 @@ function updater(){
     let listTitle = []
     for(let i of listTitleData){
         listTitle.push(i.value)
-        console.log(i.value)
     }
     let listDescription = []
     for(let i of listDescriptionData){
         listDescription.push(i.value)
-        console.log(i.value)
     }
 
     const dropContent = document.querySelector('#final-items')   
@@ -131,7 +201,7 @@ function updater(){
                                 
                         <div class="drop__data">
                             <div>
-                                <h1 class="drop__name">${listTitle[i]}</h1>
+                                <h1 class="drop__name"><i class="ri-star-line"></i> ${listTitle[i]}</h1>
                                 <span class="drop__profession">${listDescription[i]}</span>
                             </div>
                         </div>
@@ -203,5 +273,74 @@ function deleteDrop(){
         deleteItem.parentNode.removeChild(deleteItem)
     }
     
+
+}
+
+
+
+var windowHeight = window.innerHeight;
+document.body.style.height = windowHeight + "px";
+
+
+
+
+let areaCv = document.querySelector('html')
+
+let resumeButton = document.querySelector('.downloadfile')
+
+let opt = {
+    margin:       -1.5,
+   
+    filename:     'WishList.pdf',
+    image:        { type: 'jpeg', quality: 0.98 },
+    html2canvas:  { scale: 4 },
+    jsPDF:        { format: 'a4', orientation: 'portrait' },
+    enableLinks: true,
+    pagebreak: { mode: ['avoid-all', 'css', 'legacy'] }
+    
+  };
+
+
+function generateResume(){
+    html2pdf(areaCv, opt)
+}
+
+resumeButton.addEventListener('click', () => {
+    document.querySelector('.resultcontrols').style.transform = 'scale(0)'
+    document.querySelector('.resultdrop').style.marginTop = '3rem'
+
+
+    generateResume()
+    setTimeout(() => {
+        reverseControls()
+    }, 3900);
+
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3639,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+          toast.addEventListener('mouseenter', Swal.stopTimer)
+          toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+      })
+      
+      Toast.fire({
+        icon: 'success',
+        title: 'Download Initiated'
+      })
+      document.querySelector('.swal2-popup').style.background = '#1b1a1a'
+      document.querySelector('.swal2-popup').style.color = 'white'
+      document.querySelector('.swal2-timer-progress-bar').style.background = '#bebcc5'
+      document.querySelector('.swal2-success-circular-line-left').style.background = '#1b1a1a' 
+      document.querySelector('.swal2-success-circular-line-right').style.background = '#1b1a1a' 
+      document.querySelector('.swal2-success-fix').style.background = '#1b1a1a' 
+
+      
+})
+function reverseControls(){
+    document.querySelector('.resultcontrols').style.transform = 'scale(1)'
 
 }
